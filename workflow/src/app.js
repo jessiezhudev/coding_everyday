ac979fbcb95c438c2010d9c54682f1a11bcadc3e
jessie.zhu <zhuyudian@zhuyudiandeMacBook-Pro.local>
2017-03-22 13:50:58 +0800

    add new

---
+++ workflow/src/app.js
@@ -0,0 +1,6 @@
+import layer from './components/layer/layer.js'
+
+const App = function() {
+  console.log(layer)
+}
+new App()
--- workflow/webpack.config.js
+++ workflow/webpack.config.js
@@ -1,11 +1,17 @@
+var htmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  entry: {
-    main: './src/scripts/main.js',
-    side: './src/scripts/a.js'
-  },
+  entry: './src/app.js'
   output: {
     path: './dist',
-    filename: '[name]-[chunkhash].js'
-  }
+    filename: 'js/[name].bundle.js'
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
 }
 //chunkhash不同于hash值，是单个文件的版本号
